c03d5629683995b68d09bd0b45b186805a91f239
nabiilahputri13 <putrisafa1394@gmail.com>
2025-07-01 23:20:57 +0700

    tugas hari-2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import RootLayout from '@/layout'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  const metaTitle = router.pathname === '/' ? 'home' : router.pathname.replace('/', '')
+
+  return (
+    <RootLayout metaTitle={metaTitle}>
+      <Component {...pageProps}/>
+    </RootLayout>
+  )
 }
